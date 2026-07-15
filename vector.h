4a2c13308b6932cf897a4e3defc9517cac8f30ca
Renan M. Evaristo <devrme01@gmail.com>
2026-07-15 20:16:17 -0300

    Rename and update vector functions in vector.h

--- vector.h
+++ vector.h
@@ -10,9 +10,9 @@
 #include "node.h"
 #include "util.h"
 
-void vector_resize(vecpeople *vector);
+void create_vector_resized(vecpeople *vector);
 bool verify_vector_size(vecpeople *vector);
-void vector_reindex(vecpeople *vector);
+void vector_reindex2(vecpeople *vector, vecpeople *copy_vector, int old_cap);
 void vector_add_data(vecpeople *vector, int index, person *new_person);
 void vector_free(vecpeople *vector);
 bool vector_search(vecpeople *vector, person *person_search);
