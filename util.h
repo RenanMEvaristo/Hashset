f3c43c969bae91e2b50476f198751d29cc457481
Renan M. Evaristo <devrme01@gmail.com>
2026-07-15 20:16:06 -0300

    Add new function declarations to util.h

--- util.h
+++ util.h
@@ -11,6 +11,9 @@ int get_index(int hash, int capacity);
 void print_data(vecpeople *my_vector);
 void add_data(vecpeople *vector, person *person_data);
 int create_hash_get_index(vecpeople *vector, person *person);
-void print_only_nodes(vecpeople *vector);
 void copy_vector_elements(vecpeople *vector, vecpeople *copy_vector, int old_cap);
+void print_only_nodes(vecpeople *vector);
+void generate_random_data(person *p);
+void save_time_to_file(const char *label, double time_ms);
+
 #endif
